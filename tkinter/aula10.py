45799a3a3de7d4d67cb241fc8f2f56bb50c6ac87
Gabriel Félix <gfedacs@hotmail.com>
2023-08-20 18:40:39 -0300

    Update aula10.py

--- tkinter/aula10.py
+++ tkinter/aula10.py
@@ -9,10 +9,10 @@ lb1 = Label (janela, text='Horizontal',bg ='white')
 lb2 = Label (janela, text='',bg ='black')
 lb3 = Label (janela, text='',bg ='black')
 
-lb1.pack(side = TOP,fill = X)
+lb1.pack(side = TOP,fill = BOTH,expand=1)
 
 
-lb2.pack(side = LEFT,fill = Y)
+lb2.pack(side = LEFT,fill = X)
 
 lb3.pack(side = RIGHT,fill = Y)
 
